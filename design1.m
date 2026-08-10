1c68b3fcc6e5158b38899f307de328a7c503ca09
caio-ishida <caioschaden@gmail.com>
2026-08-10 20:59:44 -0300

    agora sim!

--- design1.m
+++ design1.m
@@ -31,36 +31,43 @@ dlgtitle = 'Dados do valor a ser otimizado';
 fieldsize = [1 45; 1 45];
 in = inputdlg(prompt, dlgtitle, fieldsize);
 
-delta = zeros(2);
+if ~isempty(in)
+    resultado.Nome = in{1};
+    resultado.Unidade = in{2};
+end
+
+delta = zeros(1, 2);
 delta(1) = (variaveis(1).Max - variaveis(1).Min)/2;
 delta(2) = (variaveis(2).Max - variaveis(2).Min)/2;
 
-B1 = variaveis(1).Min .* ones(9, 1);
+B1 = zeros(9, 1);
 for i = 1:3
     for j = 1:3
-        B1(3 * i + j - 3) = B1(3 * i + j - 3) + (i - 1) * delta(1);
+        B1(3 * i + j - 3) =  i - 2;
     end
 end
 
-B2 = variaveis(2).Min .* ones(9, 1);
+B2 = zeros(9, 1);
 for i = 1:3
     for j = 1:3
-        B2(3 * i + j - 3) = B2(3 * i + j - 3) + (j - 1) * delta(2);
+        B2(3 * i + j - 3) = j - 2;
     end
 end
 
 prompt = sprintf('Realize os seguintes ensaios em ordem aleatória:');
 for i = 1:9
-    prompt = [prompt, sprintf('\n%d. %s = %f %s, %s = %f %s', i, variaveis(1).Nome, B1(i), variaveis(1).Unidade, variaveis(2).Nome, B2(i), variaveis(2).Unidade)];
+    aux1 = B1(i) * delta(1) + variaveis(1).Min + delta(1);
+    aux2 = B2(i) * delta(2) + variaveis(2).Min + delta(2);
+    prompt = [prompt, sprintf('\n%d. %s = %f %s, %s = %f %s', i,variaveis(1).Nome, aux1, variaveis(1).Unidade, variaveis(2).Nome, aux2, variaveis(2).Unidade)];
 end
 questdlg(prompt, 'Ensaios', 'Ok', 'Ok');
 
-aux1 = B1 .* B1;
-aux2 = B2 .* B2;
-aux3 = B1 .* B2;
+aux3 = B1 .* B1;
+aux4 = B2 .* B2;
+aux5 = B1 .* B2;
 
 A = ones(9, 1);
-X = [A , B1 , B2 , aux1 , aux2 , aux3];
+X = [A , B1 , B2 , aux3 , aux4 , aux5];
 
 Y = zeros(9, 1);
 for i = 1:9
@@ -75,25 +82,30 @@ b = (X' * X) \ (X' * Y);
 resposta = modelo(B1(5), B2(5), b);
 
 % Andando no superfície de resposta
-aux4 = b .* b;
-pivot = sqrt(min(aux4));
+[passo_base, k] = min(delta .* delta);
+passo_base = sqrt(passo_base) / 10;
 for i = 1:2
-    caminho(i) = delta(i) / pivot;
+    caminho(i) = passo_base * b(i + 1) / b(k + 1);
 end
-max = resposta;
-passos = 1;
-x1_in_range = variaveis(1).Min <= B1(5) + passos * caminho(1) && B1(5) + passos * caminho(1) <= variaveis(1).Max;
-x2_in_range = variaveis(2).Min <= B2(5) + passos * caminho(2) && B2(5) + passos * caminho(2) <= variaveis(2).Max;
-while (resposta >= max && x1_in_range && x2_in_range)
-    resposta = modelo(B1(5) + passos * caminho(1), B2(5) + passos * caminho(2), b);
-    x1_in_range = variaveis(1).Min <= B1(5) + passos * caminho(1) && B1(5) + passos * caminho(1) <= variaveis(1).Max;
-    x2_in_range = variaveis(2).Min <= B2(5) + passos * caminho(2) && B2(5) + passos * caminho(2) <= variaveis(2).Max;
-    passos = passos + 1;
+
+valor_max = resposta;
+passos = 0;
+x1 = B1(5);
+x2 = B2(5);
+x1_in_range = (-1 <= x1) && (x1 <= 1);
+x2_in_range = (-1 <= x2) && (x2 <= 1);
+while (resposta >= valor_max && x1_in_range && x2_in_range)
+    valor_max = resposta;
+    passos= passos + 1;
+    x1 = B1(5) + passos * caminho(1);
+    x2 = B2(5) + passos * caminho(2);
+    resposta = modelo(x1, x2, b);
+    x1_in_range = (-1 <= x1) && (x1 <= 1);
+    x2_in_range = (-1 <= x2) && (x2 <= 1);
 end
 resposta = modelo(B1(5) + (passos - 1) * caminho(1), B2(5) + (passos - 1) * caminho(2), b);
+x1 = x1 * delta(1) + caminho(1);
+x2 = x2 * delta(2) + caminho(2);
 
-
-prompt = sprintf('%f + %f x(1) + %f x(2) + %f (x1)² + %f (x2)² + %f (x1)(x2)', b(1), b(2), b(3), b(4), b(5), b(6));
-questdlg(prompt, 'Ok', 'Ok');
-prompt = sprintf('O valor maximo de y eh %f', resposta);
-questdlg(prompt);
+prompt = sprintf('O %s maximo eh %f %s quando\nx1 = %f, x2 = %f', resultado.Nome, resposta, resultado.Unidade, x1, x2);
+questdlg(prompt, 'Valor maximo', 'Ok', 'Ok');
