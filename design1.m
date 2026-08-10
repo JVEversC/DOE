b503a03a33a6b3f9e14928d3607338bde5e7e87d
caio-ishida <caioschaden@gmail.com>
2026-08-10 16:57:21 -0300

    opa

--- design1.m
+++ design1.m
@@ -6,48 +6,46 @@ resultado = Variavel();
 prompt = {'Nome da primeira variável', 'Unidade da primeira variável', 'Limite inferior da primeira variável', 'Limite superior da primeira variável'};
 dlgtitle = 'Dados da primeira variável';
 fieldsize = [1 45; 1 45; 1 45; 1 45];
-ans = inputdlg(prompt, dlgtitle, fieldsize);
+in = inputdlg(prompt, dlgtitle, fieldsize);
 
-if ~isempty(ans)
-    variaveis(1).Nome = ans{1};
-    variaveis(1).Unidade = ans{2};
-    variaveis(1).Min = str2double(ans{3});
-    variaveis(1).Max = str2double(ans{4});
+if ~isempty(in)
+    variaveis(1).Nome = in{1};
+    variaveis(1).Unidade = in{2};
+    variaveis(1).Min = str2double(in{3});
+    variaveis(1).Max = str2double(in{4});
 end
 
 prompt = {'Nome da segunda variável', 'Unidade da segunda variável', 'Limite inferior da segunda variável', 'Limite superior da segunda variável'};
 dlgtitle = 'Dados da segunda variável';
-ans = inputdlg(prompt, dlgtitle, fieldsize);
+in = inputdlg(prompt, dlgtitle, fieldsize);
 
-if ~isempty(ans)
-    variaveis(2).Nome = ans{1};
-    variaveis(2).Unidade = ans{2};
-    variaveis(2).Min = str2double(ans{3});
-    variaveis(2).Max = str2double(ans{4});
+if ~isempty(in)
+    variaveis(2).Nome = in{1};
+    variaveis(2).Unidade = in{2};
+    variaveis(2).Min = str2double(in{3});
+    variaveis(2).Max = str2double(in{4});
 end
 
 prompt = {'Nome do valor a ser otimizado', 'Unidade do valor a ser otimizado'};
 dlgtitle = 'Dados do valor a ser otimizado';
 fieldsize = [1 45; 1 45];
-ans = inputdlg(prompt, dlgtitle, fieldsize);
+in = inputdlg(prompt, dlgtitle, fieldsize);
 
 delta = zeros(2);
 delta(1) = (variaveis(1).Max - variaveis(1).Min)/2;
 delta(2) = (variaveis(2).Max - variaveis(2).Min)/2;
 
-mydialog
-
 B1 = variaveis(1).Min .* ones(9, 1);
 for i = 1:3
     for j = 1:3
-        B1(i, j) = B(i, j) + (j - 1) * delta(1)/2;
+        B1(3 * i + j - 3) = B1(3 * i + j - 3) + (i - 1) * delta(1);
     end
 end
 
 B2 = variaveis(2).Min .* ones(9, 1);
-for i = 1:9
-    for j = 1:3;
-        B2(i, j) = B(i, j) + (i - 1) * delta(2)/2;
+for i = 1:3
+    for j = 1:3
+        B2(3 * i + j - 3) = B2(3 * i + j - 3) + (j - 1) * delta(2);
     end
 end
 
@@ -62,32 +60,40 @@ aux2 = B2 .* B2;
 aux3 = B1 .* B2;
 
 A = ones(9, 1);
-X = [A ; B1 ; B2; aux1; aux2; aux3];
+X = [A , B1 , B2 , aux1 , aux2 , aux3];
 
 Y = zeros(9, 1);
 for i = 1:9
     prompt = sprintf('Resultado do ensaio %d', i);
-    ans = inputdlg(prompt, 'Resultados', [1 45]);
-    if ~isempty(ans)
-        Y(i) = str2double(ans{1});
+    in = inputdlg(prompt, 'Resultados', [1 45]);
+    if ~isempty(in)
+        Y(i) = str2double(in{1});
     end
 end
 
 b = (X' * X) \ (X' * Y);
-resposta = modelo(B1(2, 2), B2(2, 2), b);
+resposta = modelo(B1(5), B2(5), b);
+
 % Andando no superfície de resposta
 aux4 = b .* b;
-pivot = sqrt(min(c));
+pivot = sqrt(min(aux4));
 for i = 1:2
     caminho(i) = delta(i) / pivot;
 end
 max = resposta;
 passos = 1;
-while resposta >= max
-    resposta = modelo(B1(2, 2) + passos * caminho(1), B2(2, 2) + passos * caminho(2));
+x1_in_range = variaveis(1).Min <= B1(5) + passos * caminho(1) && B1(5) + passos * caminho(1) <= variaveis(1).Max;
+x2_in_range = variaveis(2).Min <= B2(5) + passos * caminho(2) && B2(5) + passos * caminho(2) <= variaveis(2).Max;
+while (resposta >= max && x1_in_range && x2_in_range)
+    resposta = modelo(B1(5) + passos * caminho(1), B2(5) + passos * caminho(2), b);
+    x1_in_range = variaveis(1).Min <= B1(5) + passos * caminho(1) && B1(5) + passos * caminho(1) <= variaveis(1).Max;
+    x2_in_range = variaveis(2).Min <= B2(5) + passos * caminho(2) && B2(5) + passos * caminho(2) <= variaveis(2).Max;
+    passos = passos + 1;
 end
+resposta = modelo(B1(5) + (passos - 1) * caminho(1), B2(5) + (passos - 1) * caminho(2), b);
 
 
 prompt = sprintf('%f + %f x(1) + %f x(2) + %f (x1)² + %f (x2)² + %f (x1)(x2)', b(1), b(2), b(3), b(4), b(5), b(6));
 questdlg(prompt, 'Ok', 'Ok');
-
+prompt = sprintf('O valor maximo de y eh %f', resposta);
+questdlg(prompt);
