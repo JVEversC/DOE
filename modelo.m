6ce438a99c5a5c557fb3367ec980c6b3fe36569f
caio-ishida <caioschaden@gmail.com>
2026-08-10 22:10:02 -0300

    FOIDESGRACAAAAAAA

--- modelo.m
+++ modelo.m
@@ -1,8 +1,3 @@
 function resposta = modelo(x1, x2, b)
-    resposta = 0;
-    for i = 1:3
-        for j = 1:2
-            resposta = resposta + b(i + j) * x1^j * x2^(2 - j);
-        end
-    end
+    resposta = b(1) + b(2) * x1 + b(3) * x2 + b(4) * x1 * x1 + b(5) * x2 * x2 + b(6) * x1 * x2;
 end
